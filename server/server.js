b7ad9b3116e6294d02626633203448509c2dc956
adityakhade321 <adityakhadeak47@gmail.com>
2024-05-23 12:20:16 +0530

    static middleware

--- server/server.js
+++ server/server.js
@@ -14,6 +14,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'upload')));
 
 //middleware for fetching user
 const fetchuser=async (req,res,next)=>{
